4991f9b9bf048926aaf17e858b02c9ffa44a1ac2
huy.nguyenvan <huy.nguyenvan@sutrixsolutions.com>
2017-11-08 19:28:38 +0700

    commit file

---
+++ Ecommerce/Ecommerce.Web/common/Helper/ckfinder/ConnectorConfig.cs
@@ -0,0 +1,63 @@
+﻿using CKSource.CKFinder.Connector.Config;
+using CKSource.CKFinder.Connector.Core.Builders;
+using CKSource.CKFinder.Connector.Host.Owin;
+using CKSource.FileSystem.Local;
+using Owin;
+
+namespace Ecommerce.Web.common.Helper.ckfinder
+{
+    public class ConnectorConfig
+    {
+        public static void RegisterFileSystems()
+        {
+            FileSystemFactory.RegisterFileSystem<LocalStorage>();
+            //FileSystemFactory.RegisterFileSystem<DropboxStorage>();
+            //FileSystemFactory.RegisterFileSystem<AmazonStorage>();
+            //FileSystemFactory.RegisterFileSystem<AzureStorage>();
+        }
+
+        public static void SetupConnector(IAppBuilder builder)
+        {
+            var allowedRoleMatcherTemplate = "ABC";//ConfigurationManager.AppSettings["ckfinderAllowedRole"];
+            var authenticator = new RoleBasedAuthenticator(allowedRoleMatcherTemplate);
+
+            var connectorFactory = new OwinConnectorFactory();
+            var connectorBuilder = new ConnectorBuilder();
+            var connector = connectorBuilder
+                //.LoadConfig()
+                .SetAuthenticator(authenticator)
+                .SetRequestConfiguration(
+                    (request, config) =>
+                    {
+                        //config.LoadConfig();
+
+                        //var defaultBackend = config.GetBackend("default");
+                        //var keyValueStoreProvider = new FileSystemKeyValueStoreProvider(defaultBackend);
+                        //config.SetKeyValueStoreProvider(keyValueStoreProvider);
+                        config.AddProxyBackend("local", new LocalStorage(@"MyFiles"));
+                        config.AddResourceType("Files", resourceBuilder => resourceBuilder.SetBackend("local", "files"));
+                        //config.AddAclRule(new AclRule(
+                        //    new StringMatcher("*"), new StringMatcher("/"), new StringMatcher("*"),
+                        //    new Dictionary<Permission, PermissionType>
+                        //    {
+                        // { Permission.FolderView, PermissionType.Allow },
+                        // { Permission.FolderCreate, PermissionType.Allow },
+                        // { Permission.FolderRename, PermissionType.Allow },
+                        // { Permission.FolderDelete, PermissionType.Allow },
+
+                        // { Permission.FileView, PermissionType.Allow },
+                        // { Permission.FileCreate, PermissionType.Allow },
+                        // { Permission.FileRename, PermissionType.Allow },
+                        // { Permission.FileDelete, PermissionType.Allow },
+
+                        // { Permission.ImageResize, PermissionType.Allow },
+                        // { Permission.ImageResizeCustom, PermissionType.Allow }
+                        //    }));
+
+                    })
+                .Build(connectorFactory);
+
+            builder.UseConnector(connector);
+        }
+    }
+}
---
+++ Ecommerce/Ecommerce.Web/common/Helper/ckfinder/RoleBasedAuthenticator.cs
@@ -0,0 +1,53 @@
+﻿using CKSource.CKFinder.Connector.Core;
+using CKSource.CKFinder.Connector.Core.Acl;
+using CKSource.CKFinder.Connector.Core.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Ecommerce.Web.common.Helper.ckfinder
+{
+    public class RoleBasedAuthenticator: IAuthenticator
+    {
+        private readonly string _allowedRoleMatcherTemplate;
+
+        private readonly StringMatcher _allowedRoleMatcher;
+
+        public RoleBasedAuthenticator(string allowedRoleMatcherTemplate)
+        {
+            _allowedRoleMatcherTemplate = allowedRoleMatcherTemplate;
+            _allowedRoleMatcher = new StringMatcher(allowedRoleMatcherTemplate);
+        }
+
+        public Task<IUser> AuthenticateAsync(ICommandRequest commandRequest, CancellationToken cancellationToken)
+        {
+            var claimsPrincipal = commandRequest.Principal as ClaimsPrincipal;
+
+            var roles = new string[] { };
+            if (claimsPrincipal != null && claimsPrincipal.Claims != null)
+            {
+                roles = claimsPrincipal.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).ToArray();
+            }
+
+            var user = new User(IsAuthenticated(roles), roles);
+            return Task.FromResult((IUser)user);
+        }
+
+        private bool IsAuthenticated(string[] roles)
+        {
+            //if (_allowedRoleMatcherTemplate == string.Empty)
+            //{
+            //    return false;
+            //}
+
+            //var safeRoles = roles.Any() ? roles : new[] { string.Empty };
+
+            //return safeRoles.Any(role => _allowedRoleMatcher.IsMatch(role));
+            return true;
+        }
+    }
+}
